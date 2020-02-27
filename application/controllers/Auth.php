160e750c9c985873648764d4561690820b3fc772
iqbalmahatmaputra <iqbal@alumni.pcr.ac.id>
2020-02-27 19:47:59 +0700

    regis

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -24,16 +24,45 @@ class Auth extends CI_Controller {
 	}
 	public function index()
 	{
-		$data['pengguna'] = $this->M_pengguna->show_all()->result();
+		$data['title'] = 'Login Page!';
 		$this->load->view('templates/topbar',$data);
 		$this->load->view('auth/index',$data);
 		$this->load->view('templates/footer',$data);
     
 	}
-	function tambah(){
-		$data['judul'] = "Form Penambahan Data Mahasiswa";
-		$this->load->view('TambahData',$data);
+	function register(){
+		
+   
+			$this->form_validation->set_rules('name', 'Name', 'required|trim');
+			$this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]', [
+				'is_unique' => 'Email ini sudah pernah daftar'
+			]);
+			$this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[3]|matches[password2]',['matches' => 'Password Kok Beda!','min_length' => 'Password pendek kali!']);
+			$this->form_validation->set_rules('password2', 'Password', 'required|trim|matches[password1]');
+				
+			
+					if( $this->form_validation->run() == false) {
+					$data['title'] = 'Registrasi Akun';
+					$this->load->view('templates/topbar', $data);
+					$this->load->view('auth/regis');
+					$this->load->view('templates/footer');
+					} else {
+						$data = [
+							'name' => htmlspecialchars($this->input->post('name', true)),
+							'email' => htmlspecialchars($this->input->post('email', true)),
+							'image' =>'default.jpg',
+							'password' => password_hash($this->input->post('password1'),PASSWORD_DEFAULT),
+							'role_id' => 2,
+							'is_active' => 1,
+							'date_created' => date('Y\-m\-d\ H:i:s A')
+						];
+					$this->db->insert('user', $data);
+					$this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Selamat Gan! Akun sudah berhasil dibuat!</div> ');
+					redirect('auth');
+					}
+
 	}
+	
 	public function insertData(){
 		// $data['namaMatkul'] = $this->M_mahasiswa->getNamaMatkul();
 		$data['namaMatkul'] = $this->db->get('matkul')->result_array();
--- application/views/auth/regis.php
+++ application/views/auth/regis.php
@@ -1,93 +1,47 @@
-<!doctype html>
-<html lang="en">
 
-<head>
-	<!-- Required meta tags -->
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-	<!-- Bootstrap CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-		integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <div class="container">
 
-	<title>Hello, world!</title>
-</head>
-
-<body>
-<div class="row d-flex justify-content-center mt-5">
-<div class="col-6">
-	<div class="card">
-		<div class="card-body">
-		<div class="card-titile">
-			<h3>Form Ubah Data Mahasiswa</h3>
-			<hr>
-		</div>
-		<?php foreach ($mahasiswa as $u) {
+<div class="card o-hidden border-0 shadow-lg my-5 col-lg-7 mx-auto">
+  <div class="card-body p-0">
+	<!-- Nested Row within Card Body -->
+	<div class="row">
+	  <div class="col-lg">
+		<div class="p-5">
+		  <div class="text-center">
+			<h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
+		  </div>
+		  <form class="user" method="post" action="<?= base_url('auth/register'); ?>">
+			<div class="form-group">
+			  <input type="text" class="form-control form-control-user" id="name" placeholder="Nama Lengkap" name="name" value="<?= set_value('name'); ?>">
+			<?= form_error ('name','<small class="text-danger pl-3">','</small>'); ?></div>
+			<div class="form-group">
+			  <input type="text" class="form-control form-control-user" id="email" placeholder="Email Address" name="email" value="<?= set_value('email'); ?>">
+			<?= form_error ('email','<small class="text-danger pl-3">','</small>'); ?></div>
+			<div class="form-group row">
+			  <div class="col-sm-6 mb-3 mb-sm-0">
+				<input type="password" class="form-control form-control-user" id="password1" name="password1" placeholder="Password" name="password">
+			   <?= form_error ('password1','<small class="text-danger pl-3">','</small>'); ?></div>
+			  <div class="col-sm-6">
+				<input type="password" class="form-control form-control-user" id="password2" name="password2" placeholder="Repeat Password">
+			  </div>
+			</div>
+			<button type="submit" class="btn btn-primary btn-user btn-block">
+			  Register Account
+			</button>
 			
-		?>
-			<form action=<?php echo site_url('mahasiswa/update'); ?> method="post">
-				<div class="container">
-					<div class="form-group">
-						<label for="nim">Nim</label>
-						<input type="number" class="form-control" readonly placeholder="ex: 1455301035" name="nim" id="nim" value=<?php echo $u->nim ?>>
-					</div>
-					<div class="form-group">
-						<label for="nama">Nama</label>
-						<input type="text" class="form-control" placeholder="ex: Iqbal Mahatma" name="nama" id="nama" value=<?php echo $u->nama ?>>
-					</div>
-					<div class="form-group">
-						<label for="jk">Jenis Kelamin</label>
-						<select name="jk" id="jk" class="form-control custom-select">
-						<?php if($u->jk == 'Lelaki'){ ?>
-						<option value="Lelaki" selected>Lelaki</option>
-						<option value="Perempuan">Perempuan</option>
-						<?php }else{ ?>
-							<option value="Lelaki">Lelaki</option>
-						<option value="Perempuan" selected>Perempuan</option>
-						<?php } ?>
-						</select>
-					</div>
-					<div class="form-group">
-						<label for="alamat">Alamat</label>
-						<input type="text" class="form-control" placeholder="ex: Jl. Sekolah" name="alamat" id="alamat" value=<?php echo $u->alamat ?>>
-					</div>
-					<div class="form-group">
-						<label for="telfon">Telfon</label>
-						<input type="number" class="form-control" placeholder="ex: 08121616172" name="telfon" id="telfon" value=<?php echo $u->telfon ?>>
-					</div>
-                    <div class="form-group">
-					<label for="kode_matkul">Mata Kuliah</label>
-					<select name="kode_matkul" id="kode_matkul" class="form-control custom-select">
-							<?= $matkul=$this->db->query("SELECT DISTINCT `matkul`.*, `matkul`.`nama_matkul`
-            FROM `matkul` JOIN `mahasiswa`")->result(); ?>
-							<?php foreach($matkul as $key => $value) : 
-								if($value->kode_matkul == $u->kode_matkul){ ?>
-							<option value="<?= $value->kode_matkul ?>" selected><?= $value->nama_matkul;?></option>
-							<?php }else{ ?>
-							<option value="<?= $value->kode_matkul ?>"><?= $value->nama_matkul;?></option>
-						<?php	} endforeach; ?>
-						</select>
-						</div>
-                <input type="submit" value="Simpan" class="btn btn-success float-right">
-				</div>
-			</form>
-							<?php } ?>
+		  </form>
+		  <hr>
+		  <div class="text-center">
+			<a class="small" href="forgot-password.html">Forgot Password?</a>
+		  </div>
+		  <div class="text-center">
+			<a class="small" href="<?= base_url('auth'); ?>">Already have an account? Login!</a>
+		  </div>
 		</div>
+	  </div>
 	</div>
-</div>
+  </div>
 </div>
 
-	<!-- Optional JavaScript -->
-	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-		integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
-	</script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
-		integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
-	</script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-		integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
-	</script>
-</body>
-
-</html>
+</div>
